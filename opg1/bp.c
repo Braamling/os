6abd10e43635821c659829045e4947c3ff53cb0e
Klaplong <bas.vandenheuvel@student.uva.nl>
2013-04-04 14:40:15 +0200

    Instruction struct.

--- opg1/bp.c
+++ opg1/bp.c
@@ -13,11 +13,41 @@
 
 #include "bp.h"
 
-int execute_command(unsigned char *command_line) {
+/* Construct an instruction.
+ *
+ * Returns NULL on failure, a pointer to the newly created instruction on
+ * success. */
+instruction *create_instruction(char *command, char *arguments) {
+	instruction *instr;
+
+	instr = malloc(sizeof(instruction));
+	if (instr == NULL)
+		return NULL;
+
+	instr->command = command;
+	instr->arguments = arguments;
+
+	return instr;
+}
+
+/* Destroy an instruction.
+ *
+ * Returns 0 if the given instruction is a NULL pointer, returns 1 on
+ * success. */
+int destroy_instruction(instruction *instr) {
+	if (instr == NULL)
+		return 0;
+
+	free(instr);
+
+	return 1;
+}
+
+int execute_commands(char **commands, char *arguments) {
 	return 0;
 }
 
-int parse_command(unsigned char *command_line) {
+int parse_command(char *command_line) {
 	return 0;
 }
 
--- opg1/bp.h
+++ opg1/bp.h
@@ -1,4 +1,11 @@
 #define CHILD 0
 
-int execute_command(unsigned char *);
+typedef struct instruction_struct {
+	char *command, *arguments;
+} instruction;
+
+instruction *create_instruction(char *, char *);
+int destroy_instruction(instruction *);
+
+int execute_command(unsigned char *, unsigned char *);
 int parse_command(unsigned char *);
