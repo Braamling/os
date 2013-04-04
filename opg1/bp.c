d686af9e1a7fee423b1e69f822567c9c8a204a61
Braamling <Bram.vandenakker@student.uva.nl>
2013-04-04 14:53:56 +0200

    parse_command junk

--- opg1/bp.c
+++ opg1/bp.c
@@ -10,7 +10,6 @@
 #include <unistd.h>
 #include <ctype.h>
 #include <string.h>
-
 #include "bp.h"
 
 int execute_command(unsigned char *command_line) {
@@ -18,9 +17,50 @@ int execute_command(unsigned char *command_line) {
 }
 
 int parse_command(unsigned char *command_line) {
+	char test[39]= "test test | test tst | tet test \n";
+	unsigned char *args[10] = {NULL}; 
+					 /* This is not really elegant, but
+					given the fact that we have a limited
+					input-line length, it cannot contain
+					more than half as many arguments as
+					characters */
+	char *temp_begin, *temp_end;
+	int i = 0;
+
+	if(args[i++] = strtok(test, "|")){
+		while(args[i++] = strtok(NULL, "|"));
+	}
+	args[i] = strtok(NULL, "\n");
+
+	int j;
+
+	for (j = 0; j < i-1; j++)
+	{
+		printf("%s \n", strtok(args[j], " "));
+		printf("%s \n", strtok(NULL, "\0"));
+	}
+
+
+	//temp_end = strtok(test, "|");
+	//temp_begin = strtok(NULL, "|");
+
+/*	while(temp = strtok(NULL, "|")){
+		args[i++] = strtok(temp, " ");
+		args[i++] = strtok(NULL, " ");
+	}
+	if(temp = strtok(NULL, "\n")){
+		args[i++] = strtok(temp, " ");
+		args[i++] = strtok(NULL, " ");
+	}*/
+
+	printf("%s, %s, %s", args[0], args[1], args[2]);
+	//strtok(test, '|');
+
 	return 0;
 }
 
 int main(int argc, char *argv[]) {
+
+
 	return 0;
 }
