73808ab17d38eb386e411e7538496859225297c7
Braamling <Bram.vandenakker@student.uva.nl>
2013-04-04 13:40:59 +0200

    header file added

---
+++ opg1/bp.h
@@ -0,0 +1,26 @@
+/* Shell / bp.c
+ *
+ * Authors: 
+ * -Bas van den Heuvel
+ * -Bram van den Akker
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <string.h>
+
+int CHILD = 0;
+
+int execute_command(unsigned char *commandStr) {
+
+}
+
+int parse_command(unsigned char *commandStr){
+	
+}
+
+int main(int argc, char *argv[]) {
+
+}
