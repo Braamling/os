8973f6e2c2886aa56eeb0b758641f458e44091d1
Braamling <Bram.vandenakker@student.uva.nl>
2013-04-13 17:57:43 +0200

    introduction commentairy

--- opg1/bp.c
+++ opg1/bp.c
@@ -1,5 +1,26 @@
 /* Shell / bp.c
  *
+ * The following program is a linux command shell. 
+ * Bp has the following features:
+ * - Parsing commands to allow the use of arguments
+ * - Piping command output to other commands.
+ * - Using the readline/history package to easily reuse used commands
+ * - Catching keyboard sigint calls and redirecting them in the parent proces
+ * - 
+ *
+ * Readline/history.h might not work in the first place. In this case please
+ * download readline by using the follow command in the terminal 
+ * (debian based systems only):
+ * 
+ * sudo apt-get install libreadline6 libreadline6-dev
+ * 
+ * The following files are needed to run the shell:
+ * bp.h, bp.c and (optional) Makefile.
+ *
+ * The shell can be exited by typing 'exit' when prompted for input.
+ * ^C will not exit the shell therefore in case of an emergency ^D can be used 
+ * to close the shell. 
+ * 
  * Authors: 
  * -Bas van den Heuvel
  * -Bram van den Akker
