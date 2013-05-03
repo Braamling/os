ec0577cb5796921b28494ae8ddf631be461d09f2
Klaplong <bas.vandenheuvel@student.uva.nl>
2013-05-03 11:45:16 +0200

    Code opmaak.

--- opg3/mem_alloc.h
+++ opg3/mem_alloc.h
@@ -1,53 +1,35 @@
-/* Dit is de mem_alloc.h file zoals beloofd in de practicumhandleiding */
-/*                                                                     */
-/*   Auteur:	Dick van Albada                                        */
-/*		Faculteit Wiskunde en Informatica		       */
-/*		Universiteit van Amsterdam			       */
-/*   Datum:	5 oktober 1993                                         */
-/*   Versie:	0.01                                                   */
-/* 								       */
+/* Dit is de mem_alloc.h file zoals beloofd in de practicumhandleiding.
+ *
+ * Auteur: Dick van Albada
+ * Faculteit Wiskunde en Informatica
+ * Universiteit van Amsterdam
+ * Datum: 5 oktober 1993
+ * Versie: 0.01 */
 
-/* Constanten */
+#define MEM_SIZE (32768)
 
-#define MEM_SIZE	(32768)
-
-/* Procedures */
-
+/* mem_init wordt aangeroepen voor enige andere procedure uit deze file wordt
+ * gebruikt. Hij initialseert de memory-manager en zorgt ervoor dat het te
+ * beheren geheugen daar bekend is. */
 void mem_init(long mem[MEM_SIZE]);
 
-/* mem_init wordt aangeroepen voor enige andere procedure uit deze file
-   wordt gebruikt. Hij initialseert de memory-manager en zorgt ervoor
-   dat het te beheren geheugen daar bekend is
-   */
-
+/* mem_get alloceert een stuk geheugen ter grootte van request - indien
+ * mogelijk en geeft de index van het eerste element van dat stuk geheugen
+ * terug. Indien de aanvraag niet gehonoreerd kan worden, wordt -1
+ * teruggegeven. */
 long  mem_get(long request);
 
-/* mem_get alloceert een stuk geheugen ter grootte van request - indien
-   mogelijk en geeft de index van het eerste element van dat stuk geheugen
-   terug.
-   Indien de aanvraag niet gehonoreerd kan worden, wordt -1 teruggegeven
-   */
-
+/* mem_free wordt aangeroepen om een eerder verkregen stuk geheugen op locatie
+ * index weer vrij te geven. Index moet een geldige waarde hebben. */
 void mem_free(long index);
 
-/* mem_free wordt aangeroepen om een eerder verkregen stuk geheugen op
-   locatie index weer vrij te geven. Index moet een geldige waarde
-   hebben
-   */
-
+/* mem_available vertelt de gebruiker hoeveel geheugen er nog beschikbaar is:
+ * -empty: totale hoeveelheid vrije ruimte.
+ * -large: omvang van het grootste gat, gecorrigeerd voor administratie.
+ * -n_holes: het aantal gaten. */
 void mem_available(long *empty, long *large, long *n_holes);
 
-/* mem_available vertelt de gebruiker hoeveel geheugen er nog
-   beschikbaar is
-   empty:	totale hoeveelheid vrije ruimte
-   large:	omvang van het grootste gat, gecorrigeerd voor
-		administratie
-   n_holes:	het aantal gaten
-   */
-
+/* mem_exit wordt alleen als laatste routine aangeroepen en beeindigt het
+ * gebruik van de memory manager. Deze routine zorgt zonodig voor opruimen en
+ * afronden. */
 void mem_exit();
-
-/* mem_exit wordt alleen als laatste routine aangeroepen en beeindigt
-   het gebruik van de memory manager.
-   Deze routine zorgt zonodig voor opruimen en afronden.
-   */
--- opg3/next-fit.c
+++ opg3/next-fit.c
@@ -3,23 +3,23 @@
 
 #include "mem_alloc.h"
 
-void mem_init(long mem[MEM_SIZE]){
-	
+void mem_init(long mem[MEM_SIZE]) {
+    
 }
 
-long  mem_get(long request){
-	printf("request, %ld", request);
-	return 0;
+long  mem_get(long request) {
+    printf("request, %ld", request);
+    return 0;
 }
 
-void mem_free(long index){
-	printf("test");
+void mem_free(long index) {
+    printf("test");
 }
 
-void mem_available(long *empty, long *large, long *n_holes){
-	printf("test");
+void mem_available(long *empty, long *large, long *n_holes) {
+    printf("test");
 }
 
-void mem_exit(){
-	printf("test");
+void mem_exit() {
+    printf("test");
 }
