62ab0b1746dc0720e50aad3472e386b02837d942
Klaplong <bas.vandenheuvel@student.uva.nl>
2013-05-04 13:48:47 +0200

    Some work on backend.

--- opg3/address.h
+++ opg3/address.h
@@ -1,3 +1,11 @@
+#define ADDR_START 2
+#define ADDR_MAX_INDEX 0
+#define ADDR_COUNT_INDEX 1
+
 long address_set(long index, int used);
 int address_is_used(long address);
 long get_index(long address);
+long get_free_address_slot(long *mem);
+long get_address_max(long *mem);
+long get_address_count(long *mem);
+long get_address_end(long *mem);
