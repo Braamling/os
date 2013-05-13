16e7f29063cb5e07385398b0d72425d874ec294c
Braamling <Bram.vandenakker@student.uva.nl>
2013-05-13 21:26:20 +0200

    fit commentairy

--- opg3_rev/best_fit.c
+++ opg3_rev/best_fit.c
@@ -1,11 +1,28 @@
+/* File: best_fit.c
+ * Authors: Bas van den Heuvel & Bram van den Akker
+ *
+ * This file contains the fit function for the best fit algorithme. 
+ * The function is used in mem_alloc.c */
+
 #include "fit.h"
 #include "admin.h"
 #include "mem_alloc.h"
 
+/* Find the best fit block.
+ *
+ * Arguments:
+ * -long *mem: The memory array.
+ * -long request: The requested memory size
+ *
+ * Results:
+ * -Success: The best fit block.
+ * -Failure: -1. */
 long fit(long *mem, long request){
 	long admin_index, size, best_fit_size, best_fit_index;
 
 	admin_index = FIRST_INDEX;
+
+	/* set the size and index at -1 to indicated that they are not yet found */
 	best_fit_size = -1;
 	best_fit_index = -1;
 
@@ -13,6 +30,7 @@ long fit(long *mem, long request){
 		size = get_block_size(mem, admin_index);
 
 		if (!admin_get_used(mem[admin_index]) && (size >= request)) {
+			/* If the block is the same size as the request it will be used */
 			if (size == request)
 				return admin_index;
 			if (size < best_fit_size || best_fit_size == -1) {
