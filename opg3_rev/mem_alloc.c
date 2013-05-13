46959ea7583273f604c58d9826e7276cc99c08b6
Klaplong <bas.vandenheuvel@student.uva.nl>
2013-05-13 21:08:01 +0200

    Mem alloc comments.

--- opg3_rev/mem_alloc.c
+++ opg3_rev/mem_alloc.c
@@ -1,3 +1,15 @@
+/* File: mem_alloc.c
+ *
+ * Authors: Bas van den Heuvel & Bram van den Akker
+ *
+ * This file contains all functions used by the memory simulator. It uses
+ * functionality from admin.c and the algorithms from next_fit.c and best_fit.c
+ * to manage virtual memory.
+ *
+ * The comments for each function were copied from mem_alloc.h. Since these
+ * comments weren't written by us, but merely provided for us, we did not
+ * translate them into English. */
+
 #include <stdio.h>
 
 #include "mem_alloc.h"
@@ -7,19 +19,26 @@
 static long *memory;
 long a, b, c;
 
+/* mem_init wordt aangeroepen voor enige andere procedure uit deze file wordt
+ * gebruikt. Hij initialseert de memory-manager en zorgt ervoor dat het te
+ * beheren geheugen daar bekend is. */
 void mem_init(long mem[MEM_SIZE]) {
 	memory = mem;
-
-	/* Anally rape someone. */
 
 	/* Start with one empty block. */
 	memory[0] = 1;
 	memory[1] = admin_make(0, 0);
 }
 
+/* mem_get alloceert een stuk geheugen ter grootte van request - indien
+ * mogelijk en geeft de index van het eerste element van dat stuk geheugen
+ * terug. Indien de aanvraag niet gehonoreerd kan worden, wordt -1
+ * teruggegeven. */
 long mem_get(long request) {
 	long available_admin_index;
 
+	/* Find the gap to put the request in. The algorithm used depends on with
+	 * what .o file the program is compiled. */
 	available_admin_index = fit(memory, request);
 	if (available_admin_index == -1)
 		return -1;
@@ -27,20 +46,27 @@ long mem_get(long request) {
 	if (alloc_block(memory, available_admin_index, request) == -1)
 		return -1;
 
+	/* The actual memory available starts one place from the requested block.
+	 * The first place is used for administration purposes. */
 	return available_admin_index + 1;
 }
 
+/* mem_free wordt aangeroepen om een eerder verkregen stuk geheugen op locatie
+ * index weer vrij te geven. Index moet een geldige waarde hebben. */
 void mem_free(long index) {
 	long admin_index;
 
-	if (index == 2)
-		mem_available(&a, &b, &c);
-
+	/* The index of the block's administration is one place before the memory
+	 * index that has been given to the simulator. */
 	admin_index = index - 1;
 
 	free_block(memory, admin_index);
 }
 
+/* mem_available vertelt de gebruiker hoeveel geheugen er nog beschikbaar is:
+ * -empty: totale hoeveelheid vrije ruimte.
+ * -large: omvang van het grootste gat, gecorrigeerd voor administratie.
+ * -n_holes: het aantal gaten. */
 void mem_available(long *empty, long *large, long *n_holes) {
 	long index, next_index, block_size;
 
@@ -54,6 +80,10 @@ void mem_available(long *empty, long *large, long *n_holes) {
 	printf("i\tni\tbs\tu\n");*/
 
 	while (index) {
+
+		/* Though the block size is not relevant if the block is not used, it
+		 * is requested outside the if statement to check this. This is done
+		 * for debuggin purposes (see the outcommented lines of code). */
 		block_size = get_block_size(memory, index);
 		next_index = admin_get_next_index(memory[index]);
 
@@ -76,6 +106,9 @@ void mem_available(long *empty, long *large, long *n_holes) {
 	/*printf("=======\n");*/
 }
 
+/* mem_exit wordt alleen als laatste routine aangeroepen en beeindigt het
+ * gebruik van de memory manager. Deze routine zorgt zonodig voor opruimen en
+ * afronden. */
 void mem_exit() {
 
 }
